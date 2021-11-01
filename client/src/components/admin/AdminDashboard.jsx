f7387e22b937d0135f476f5e91225c8264a53b8b
junhao-tang <junhao.tang@outlook.com>
2021-11-01 13:14:49 +0800

    Added NumberFormatter, DateFormatter components

--- client/src/components/admin/AdminDashboard.jsx
+++ client/src/components/admin/AdminDashboard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../Navbar";
 import { Box, Button } from "@mui/material";
 import TableComponent from "../TableComponent";
+// import NumberFormatter from "../NumberFormatter";
+// import DateFormatter from "../DateFormatter";
 
 const AdminDashboard = () => {
   return (
@@ -14,6 +16,14 @@ const AdminDashboard = () => {
       <Box>
         <TableComponent />
       </Box>
+      {/* <NumberFormatter
+        inputVal={100000}
+        thousandsSeperated={true}
+        decimalPlaces={2}
+      />
+      <div>
+        <DateFormatter date={new Date()} format="dd-MMM-yyyy" />
+      </div> */}
     </div>
   );
 };
