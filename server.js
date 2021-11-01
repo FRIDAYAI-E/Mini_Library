e8101e51ab78e51dc203e2a195d328cdf1916918
ethan-su <ethansu1992@gmail.com>
2021-11-01 10:23:01 +0800

    minor edits

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const mongoose = require("mongoose");
 //* CONFIG
 //
 const MONGODB_URI =
-  process.env.MONGODB_URI ?? "mongodb://localhost:27017/alibrary";
+    process.env.MONGODB_URI ?? "mongodb://localhost:27017/alibrary";
 mongoose.connect(MONGODB_URI);
 const conn = mongoose.connection;
 conn.once("open", () => {
-  console.log(`Mongoose connection successful`);
+    console.log(`Mongoose connection successful`);
 });
 conn.on("error", (err) => {
-  console.log(`Mongoose connection failed. Err: ${err}`);
+    console.log(`Mongoose connection failed. Err: ${err}`);
 });
 
 //* MIDDLEWARE
@@ -25,12 +25,13 @@ app.use(express.json());
 
 //* TEST ROUTE
 app.get("/", (req, res) => {
-  res.send("aLibrary express working");
+    res.send("aLibrary express working");
 });
 
 //* LISTENER
 app.listen(PORT, console.log(`aLibrary listening on port ${PORT}}`));
-=======
+
+
 //* Dependecies
 const path = require('path');
 const express = require("express");
