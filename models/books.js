79700a91715fbb9decf8f25e598668bc44e3e2e2
ethan-su <ethansu1992@gmail.com>
2021-11-01 16:16:47 +0800

    book standard route completed

--- models/books.js
+++ models/books.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const booksSchema = mongoose.Schema({
     title: { type: String, required: true },
-    qty: { type: Number },
+    qty: { type: Number, min: 0 },
     genre: { type: String, required: true },
     author: { type: String, required: true },
     bookImg: { type: String },
