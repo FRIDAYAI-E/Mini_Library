74ea7c530479b259f303944c061d8ba7c423006b
ethan-su <ethansu1992@gmail.com>
2021-11-04 00:02:04 +0800

    logout completed

--- client/src/components/LogoutPage.jsx
+++ client/src/components/LogoutPage.jsx
@@ -1,29 +1,27 @@
 import * as React from "react";
-//import axios from "axios";
-//import { useState, useEffect } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 
 export default function Logout() {
-  // const [status, setStatus] = useState("idle");
+  const [status, setStatus] = useState("idle");
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setStatus("pending");
-  //     await axios.delete(`/api/session`)
-  //     .then(res => {
-  //       console.log(res);
-  //       console.log(res.data);
-  //     })
-  // }
-  //   };
-
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      setStatus("pending");
+      await axios.delete(`/api/session`).then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setStatus("completed");
+      });
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
       <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
-        You have logout
+        You have logout {status}
       </Typography>
     </div>
   );
