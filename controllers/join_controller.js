c83550cbc4d0bc0cee0b9bd0aebe5ff06480a448
junhao-tang <junhao.tang@outlook.com>
2021-11-04 18:08:31 +0800

    updated admin return books method

--- controllers/join_controller.js
+++ controllers/join_controller.js
@@ -24,13 +24,18 @@ const isAuth = (roleArr) => (req, res, next) => {
 router.get("/admin/dashboard", isAuth([SUPERUSER, ADMIN]), async (req, res) => {
   const data = [];
   for await (const b of Books.find({})) {
-    const loan = await onLoan.find({ bookID: b._id, dateReturned: "" });
+    const loan = await onLoan.find({
+      bookID: b._id,
+      $or: [{ dateReturned: "" }, { dateReturned: { $exists: false } }],
+    });
+    console.log(loan);
     const loanHistory = await onLoan.find({ bookID: b._id });
     b._doc.timesBorrowed = loanHistory.length;
     b._doc.loaned = loan.length;
     b._doc.available = b._doc.qty - loan.length;
     data.push(b);
   }
+  // $or: [{ dateReturned: "" }, { dateReturned: { $exists: false } }],
   res.json(data);
 });
 
@@ -41,16 +46,17 @@ router.get(
   async (req, res) => {
     const data = [];
     for await (const b of Books.find({})) {
-      const loan = await onLoan.find({ bookID: b._id, dateReturned: "" });
+      const loan = await onLoan.find({
+        bookID: b._id,
+        $or: [{ dateReturned: "" }, { dateReturned: { $exists: false } }],
+      });
       if (b.qty - loan.length > 0) {
         b._doc.availability = "available";
       } else {
         b._doc.availability = "unavailable";
       }
-      console.log(b);
       data.push(b);
     }
-    console.log("data", data);
     res.json(data);
   }
 );
--- controllers/onLoan_controller.js
+++ controllers/onLoan_controller.js
@@ -29,6 +29,23 @@ router.post("/", (req, res) => {
   });
 });
 
+//* GET ACTIVE LOANS
+router.get("/active", isAuth([SUPERUSER, ADMIN]), (req, res) => {
+  try {
+    onLoan.find(
+      { $or: [{ dateReturned: "" }, { dateReturned: { $exists: false } }] },
+      (err, foundonLoans) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+        }
+        res.status(200).json(foundonLoans);
+      }
+    );
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
 //* GET LOANS BY USERID
 router.get("/:id", isAuth([SUPERUSER, USER, ADMIN]), async (req, res) => {
   const { id } = req.params;
@@ -36,7 +53,6 @@ router.get("/:id", isAuth([SUPERUSER, USER, ADMIN]), async (req, res) => {
     .find({ userID: id })
     .populate("bookID")
     .exec((err, foundonLoans) => {
-      console.log("foundonLoans", foundonLoans);
       res.status(200).json(foundonLoans);
     });
 });
@@ -78,8 +94,11 @@ router.delete("/:id", isAuth([SUPERUSER]), (req, res) => {
 
 //* UPDATE SPECIFIC ENTRY (For ADMIN to enter dateReturned)
 router.put("/:id", isAuth([SUPERUSER, ADMIN]), (req, res) => {
+  const { id } = req.params;
+  console.log(id);
   onLoan.findByIdAndUpdate(
-    req.params.id,
+    // req.params.id,
+    id,
     req.body,
     { new: true },
     (err, updatedonLoans) => {
