8ff4ef5d0423df9c064341124f48ab7fa546f563
junhao-tang <junhao.tang@outlook.com>
2021-11-04 08:12:12 +0800

    join_controller api calls working

--- controllers/join_controller.js
+++ controllers/join_controller.js
@@ -1,30 +1,58 @@
 const express = require("express");
 const router = express.Router();
-const onLoans = require("../models/onLoan.js");
+const onLoan = require("../models/onLoan.js");
 const Books = require("../models/books.js");
+const async = require("async");
 
-//* ROUTER => ADMINDASHBOARD READ ROUTE
-router.get("/admin/dashboard", async (req, res) => {
-  const data = [];
-  const books = await Books.find({});
-  for (const b of books) {
-    if ((await onLoans.find({ bookID: b._doc._id }).length) > 0) {
-      b._doc.timesBorrowed = await onLoans.find({ bookID: b._doc._id }).length;
-      b._doc.loaned =
-        (await onLoans.find({ bookID: b._doc._id, dateReturned: null })
-          .length) > 0
-          ? onLoans.find({ bookID: b._doc._id, dateReturned: null }).length
-          : 0;
-      b._doc.available = b._doc.qty - b._doc.loaned;
-      data.push(b);
-    } else {
-      b._doc.timesBorrowed = 0;
-      b._doc.loaned = 0;
-      b._doc.available = b._doc.qty;
-      data.push(b);
+//* AUTH CHECK
+const SUPERUSER = "superuser";
+const ADMIN = "admin";
+const USER = "user";
+
+const isAuth = (roleArr) => (req, res, next) => {
+  if (req.session.loginUser) {
+    for (const r of roleArr) {
+      if (req.session.role === r) {
+        return next();
+      }
     }
+  }
+  res.status(404).json({ message: "Authentication required" });
+};
+
+//* GET ADMIN_DASHBOARD ROUTE
+router.get("/admin/dashboard", isAuth([SUPERUSER, ADMIN]), async (req, res) => {
+  const data = [];
+  for await (const b of Books.find({})) {
+    const loan = await onLoan.find({ bookID: b._id, dateReturned: "" });
+    const loanHistory = await onLoan.find({ bookID: b._id });
+    b._doc.timesBorrowed = loanHistory.length;
+    b._doc.loaned = loan.length;
+    b._doc.available = b._doc.qty - loan.length;
+    data.push(b);
   }
   res.json(data);
 });
+
+//* GET BROWSE_BOOK ROUTE (Users browsing books to display availability)
+router.get(
+  "/browseBooks",
+  isAuth([SUPERUSER, ADMIN, USER]),
+  async (req, res) => {
+    const data = [];
+    for await (const b of Books.find({})) {
+      const loan = await onLoan.find({ bookID: b._id, dateReturned: "" });
+      if (b.qty - loan.length > 0) {
+        b._doc.availability = "available";
+      } else {
+        b._doc.availability = "unavailable";
+      }
+      console.log(b);
+      data.push(b);
+    }
+    console.log("data", data);
+    res.json(data);
+  }
+);
 
 module.exports = router;
