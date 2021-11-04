a5eee5c64a0cfde62fdfb8ad5d8f270f52b4a4e5
ethan-su <ethansu1992@gmail.com>
2021-11-05 00:01:49 +0800

    create user page completed

--- client/src/components/users/NewUser.jsx
+++ client/src/components/users/NewUser.jsx
@@ -3,13 +3,24 @@ import axios from "axios";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+
 function NewUser() {
   const [status, setStatus] = useState("start");
   let history = useHistory();
 
   const createNewUser = async (newUser) => {
     await axios.post("/api/user", newUser).then((res) => {
-      if (res.data) {null} //empty code to prevent errors
+      if (res.data) {
+        null;
+      } //empty code to prevent errors
     });
   };
 
@@ -42,22 +53,101 @@ function NewUser() {
     };
     checkUser(userDetails);
   };
+  console.log(status);
 
   return (
-    <div>
-      <h1>Create new User </h1>
-      <form onSubmit={handleSubmit}>
-        <input name="username" placeholder="Username (required)" />
-        <input name="password" placeholder="Password (required)" />
-        <input name="email" placeholder="Email (required)" />
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h3">
+            Sign Up!
+          </Typography>
+          <Typography component="h1" variant="subtitle1">
+            Free and always will be.
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="username"
+                  name="username"
+                  required
+                  fullWidth
+                  id="username"
+                  label="username"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="current-password"
+                  name="password"
+                  required
+                  fullWidth
+                  id="password"
+                  label="password"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="email"
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="email"
+                  autoFocus
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            {status === "unavailable" ? (
+              <h5>Sorry the username/email is taken </h5>
+            ) : null}
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
 
-        <button>Create New User</button>
-      </form>
+          {/* <form onSubmit={handleSubmit}>
+            <input name="username" placeholder="Username (required)" />
+            <input name="password" placeholder="Password (required)" />
+            <input name="email" placeholder="Email (required)" />
 
-      {status === "unavailable" ? (
-        <h5>Sorry the username/email is taken </h5>
-      ) : null}
-    </div>
+            <button>Create New User</button>
+          </form>
+
+          {status === "unavailable" ? (
+            <h5>Sorry the username/email is taken </h5>
+          ) : null} */}
+        </Box>
+      </Container>
+    </>
   );
 }
 
